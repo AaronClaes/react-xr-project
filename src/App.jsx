b4e71ee71607d7f58abe679dd53a98e9a6a9b821
Aaron Claes <aaronclaes22@gmail.com>
2022-03-25 20:56:05 +0100

    feat: add useHitTest hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
+import { useRef } from "react";
 import { Box } from "@react-three/drei";
-import { ARCanvas } from "@react-three/xr";
+import { ARCanvas, DefaultXRControllers } from "@react-three/xr";
 
 const App = () => {
+  const ref = useRef();
+  useHitTest((hit) => {
+    console.log(hit);
+    hit.decompose(
+      ref.current.position,
+      ref.current.rotation,
+      ref.current.scale
+    );
+  });
   return (
     <>
-      <ARCanvas>
-        <Box>
+      <ARCanvas sessionInit={{ requiredFeatures: ["hit-test"] }}>
+        <Box ref={ref}>
           <meshBasicMaterial />
         </Box>
+        <DefaultXRControllers />
       </ARCanvas>
     </>
   );
